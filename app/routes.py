b6d1ce32197e33d8a994db82fd652e1d18c56901
D'Andre <dwalker2@ithaca.ediu>
2023-09-08 12:49:28 -0400

    This is my version 1 of my music app. For some reason I been getting an internal error when I run the program. Been trying to fix the bug.

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,20 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello Music App"
+    user = {'username': 'Dre'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/artists')
+def artists():
+    return render_template('artists.html', title='Artists')
+
+
+@app.route('/new_artists')
+def new_artists():
+    return "Under Construction. Coming Soon..."
+
+
+@app.route('/desc')
+def desc():
+    return render_template('desc.html', title='Gunna Description')
