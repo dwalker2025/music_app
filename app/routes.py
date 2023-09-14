3260b11e5fdebda5b87d4aa76220afbfd0e26764
D'Andre <dwalker2@ithaca.ediu>
2023-09-13 21:28:50 -0400

    Working music app Lab 3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash
 from app import app
+from app.forms import NewArtistForm
 
 
 @app.route('/')
@@ -15,9 +16,15 @@ def artists():
     return render_template('artists.html', title='Artists', artists=artists)
 
 
-@app.route('/new_artists')
+@app.route('/new_artists', methods=['GET', 'POST'])
 def new_artists():
-    return "Under Construction. Coming Soon..."
+    form = NewArtistForm()
+    if form.validate_on_submit():
+        flash("New Artist {} Created!".format(form.name.data))
+        new_form = NewArtistForm()
+        render_template('new_artists.html', title="New Artist", form=new_form)
+
+    return render_template('new_artists.html', title="New Artist", form=form)
 
 
 @app.route('/desc')
